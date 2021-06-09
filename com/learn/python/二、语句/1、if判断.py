89514ab33e7dfd0a79054a8f5d24b68a27946d91
anyoneElse <18201407550@163.com>
2021-06-09 21:43:47 +0800

    if判断

--- com/learn/python/二、语句/1、if判断.py
+++ com/learn/python/二、语句/1、if判断.py
@@ -10,3 +10,27 @@ else:
 代码块后面的语句。
 2. 单个 if 语句中的 expression 条件表达式可以通过布尔操作符 and ， or 和 not 实现多重条件判断。
 """
+age = 27
+if age > 30:
+    print("年龄大于三十")
+else:
+    print("年龄小于三十")
+
+
+"""
+if 语句支持嵌套，即在一个 if 语句中嵌入另一个 if 语句，从而构成不同层次的选择结构。Python 使用缩进而不是大括
+号来标记代码块边界，因此要特别注意 else 的悬挂问题。
+"""
+hi = 6
+if hi > 2:
+    if hi > 7:
+        print('好棒!好棒!')
+else:
+    print('切~')
+
+
+"""
+assert 这个关键词我们称之为“断言”，当这个关键词后边的条件为 False 时，程序自动崩溃并抛
+出 AssertionError 的异常。
+"""
+assert 2>3
