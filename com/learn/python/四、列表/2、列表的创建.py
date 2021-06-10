ec6da5197904c8805101a3aebd110a5de20a001c
lddcyl <meimima257>
2021-06-10 18:16:50 +0800

    列表

---
+++ com/learn/python/四、列表/2、列表的创建.py
@@ -0,0 +1,15 @@
+# 1. 创建一个普通列表
+x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+print(x, type(x))
+
+# 2. 利用 range() 创建列表
+x2 = list(range(1, 3))
+
+# 3. 利用推导式创建列表
+x3 = [0] ** 3
+x4 = [v for v in range(1, 3)]
+
+
+# 二维数组
+xx = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0]]
+print(x, type(x))
